87d6851657b756f0f8f394a64d27ccbf598e8a7a
diogenesmedeiros <juniormedeiros20119@gmail.com>
2024-09-12 13:50:42 -0300

    update cors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const port = 8000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: ['http://localhost:3000']
+  origin: ['http://localhost:3000', 'https://taskflicker.vercel.app']
 }));
 
 app.get('/', (req: Request, res: Response) => {
